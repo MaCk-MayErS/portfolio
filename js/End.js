6098d4eec2fb0d3f0068b72e59f8d3f8fbb35a14
MaCk-MayErS <barbary.yohan@gmail.com>
2024-12-03 16:16:44 +0100

    v6

--- js/End.js
+++ js/End.js
@@ -1,5 +1,18 @@
-document.querySelector('.endNav').addEventListener('animationiteration', function () {
-    this.style.animation = 'none';
-    this.offsetHeight; 
-    this.style.animation = 'upcredits 7s linear infinite';
+const endNav = document.querySelector('.endNav');
+const neesha = document.querySelector('.neesha');
+const presen = document.querySelector(".presentation")
+const head = document.querySelector(".image-container")
+const logo = document.getElementById("logo")
+const BtnX = document.querySelector(".bouton-dark")
+const darky = document.getElementById("close-btn")
+
+endNav.addEventListener('animationend', function() {
+  presen.style.display = "none";
+  endNav.style.display = 'none';
+  neesha.style.display = 'grid';
+  head.style.display =""
+  head.style.backgroundColor = "#000"
+  logo.style.backgroundColor = "#000"
+  BtnX.style.display = "none"
+  darky.style.display = "none"
 });
